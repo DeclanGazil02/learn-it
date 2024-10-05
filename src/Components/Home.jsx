21524708a4fe107c8cd04bd8479edd68980e9ea0
DeclanGazil02 <declan.gazil@ufl.edu>
2024-10-05 11:20:38 -0400

    Added basic layout with navbar at the top. Will change with bootstrap

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Upload your video
+      </div>
+    </>
+  )
+}
+
+export default Home
