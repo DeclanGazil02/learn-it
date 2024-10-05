8cfc0ce7808582b208a281e4fdc5c68552337839
Kevin Trinh <66029570+KTrinhGit@users.noreply.github.com>
2024-10-05 13:25:24 -0400

    Link sign in and login to each other

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 
@@ -70,6 +71,11 @@ function Login() {
           <Button variant="primary" type="submit" className="login-button">
             Create Account
           </Button>
+
+          <div className="create-account-text">
+            <p>Already have an account? <Link to="/">Login</Link>
+            </p>
+          </div>
         </Form>
       </div>
     </div>
